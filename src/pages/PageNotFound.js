097a9aec6f865b73e8ba92de570dc4498bce4955
lasyapriya-bharadwaj <lasyabharadwaj541@gmail.com>
2024-01-20 14:18:23 +0530

    Contact page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function PageNotFound() {
   return (
-    <div style={{height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: 'column', gap: 5, backgroundColor: "#ff4545"}}>
-      <h1 style={{color: "white"}}>Oops..</h1>
-      <p style={{color: "white"}}>The page you are searching for doesn't exist</p>
+    <div style={{height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: 'column', gap: 5, backgroundColor: "white"}}>
+      <h1 style={{color: "#ff5733"}}>Oops..</h1>
+      <h3 style={{color: "#ff5733"}}>The page you are searching for doesn't exist</h3>
     </div>
   )
 }
